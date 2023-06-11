f2bb5801de0029e580aa2ab6cb327416922350aa
Solomon Montagno <sjmontagno@gmail.com>
2023-06-11 16:00:58 -0400

    Whiteboarding june 11th

---
+++ June-11.js
@@ -0,0 +1,17 @@
+//Given a string representing a sentence, write a method that returns the longest word. If there’s a tie, output the longest word that’s found first
+
+
+let string = "I am Helbrand secrectly Sauron, you are a fool."
+
+const longestWord = (string) => {
+    let str = string.split(" ")
+    let longest = 0
+    let word = ""
+    str.forEach((currentWord) => {
+        if(currentWord.length > longest){
+            longest = currentWord.length
+            word = currentWord
+        }
+    });
+    return word
+}

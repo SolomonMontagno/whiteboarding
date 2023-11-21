4aa999059c99d74d7e0ef52d2ee7a5f62caf1a57
Solomon Montagno <sjmontagno@gmail.com>
2023-11-20 20:36:38 -0500

    More practice

--- Nov-11.js
+++ Nov-11.js
@@ -38,3 +38,33 @@ const linearSearch = (array, target) => {
 }
 
 console.log('palindrome', linearSearch([1,3,4,5,6,7,8,9,10,10, 11], 10 ))
+
+
+//given a string write a function that removes duplicate characters in the string keeping only the first occurrence  the function
+ let string = 'tree traversal'
+const removeDuplicates = (string) => {
+    let singleCharacterString = ''
+//loop over stirng
+for(let i = 0; i < string.length; i++){
+    if(!singleCharacterString.includes(string[i])) {
+        singleCharacterString += string[i]
+    }
+}
+//finds letter and pushes into the single character string
+//check to see if it already exists if it does it moves on and ignores it
+return singleCharacterString
+}
+
+console.log('testsingle', removeDuplicates(string))
+
+const linearSearchPractice = (array, target) => {
+//need to loop over the array to find the index of the target
+for(let i = 0; i < array.length; i++){
+    //conditional statement to check index against the target
+    if (array[i] === target) {
+        return i
+    }
+}
+    return -1
+}
+console.log("palindrome@@@@@", linearSearchPractice([1, 3, 4, 5, 6, 7, 8, 9, 10], 6));

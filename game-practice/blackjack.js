1b5f7b78b780716513aded3dee40c3e76ef5c20d
Solomon Montagno <sjmontagno@gmail.com>
2023-12-12 14:48:26 -0500

    psuedocode

--- game-practice/blackjack.js
+++ game-practice/blackjack.js
@@ -1,123 +1,121 @@
-const readline = require("readline");
+// Import the readline module for handling user input
+const readline = require('readline');
 
+// Create a readline interface for reading input from the user
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
 
+// Define a function to get a random card from a standard deck
 function getRandomCard() {
-  const cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+  // Create an array of card values from 2 to Ace
+  const cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  // Generate a random index within the length of the array
   const randomIndex = Math.floor(Math.random() * cards.length);
+  // Return the card value at the random index
   return cards[randomIndex];
 }
 
+// Define a function to calculate the score of a hand
 function calculateScore(cards) {
+  // Initialize a variable score to 0
   let score = 0;
+  // Initialize a variable hasAce to false
   let hasAce = false;
 
+  // For each card in cards:
   for (let card of cards) {
-    if (card === "A") {
+    // If the card is an Ace:
+    if (card === 'A') {
+      // Set hasAce to true
       hasAce = true;
+      // Add 11 to the score
       score += 11;
-    } else if (["K", "Q", "J"].includes(card)) {
+    } else if (['K', 'Q', 'J'].includes(card)) {
+      // Else if the card is a face card (King, Queen, Jack):
+      // Add 10 to the score
       score += 10;
     } else {
+      // Else (numeric card):
+      // Add the numeric value of the card to the score
       score += parseInt(card);
     }
   }
 
   // Handle Ace as 1 if adding 11 would cause bust
+  // While hasAce is true and the score is greater than 21:
   while (hasAce && score > 21) {
+    // Subtract 10 from the score (treat Ace as 1)
     score -= 10;
-    hasAce = false; // Consider only one Ace as 11
+    // Set hasAce to false
+    hasAce = false;
   }
 
+  // Return the final score
   return score;
 }
 
+// Define a function to display the game status
 function displayGameStatus(playerCards, dealerCards, playerScore, dealerScore, revealDealer) {
-  console.log(`\nYour cards: ${playerCards.join(", ")} (Score: ${playerScore})`);
-  console.log(`Dealer's cards: ${revealDealer ? dealerCards.join(", ") : dealerCards[0] + ", ?"}`);
+  // Print the player's cards and score
+  console.log(`\nYour cards: ${playerCards.join(', ')} (Score: ${playerScore})`);
+  // If revealDealer is true:
+  if (revealDealer) {
+    // Print the dealer's cards and score
+    console.log(`Dealer's cards: ${dealerCards.join(', ')} (Score: ${dealerScore})`);
+  } else {
+    // Else:
+    // Print only the first card of the dealer followed by a question mark
+    console.log(`Dealer's cards: ${dealerCards[0]}, ?`);
+  }
 }
 
+// Define a function to check for a Blackjack
 function checkBlackjack(playerScore, dealerScore) {
+  // If the player has a score of 21:
   if (playerScore === 21) {
-    console.log("Congratulations! You got a Blackjack!");
+    // Print a message indicating the player got a Blackjack
+    console.log('Congratulations! You got a Blackjack!');
+    // Call the endGame function
     endGame();
   } else if (dealerScore === 21) {
-    console.log("Dealer got a Blackjack. You lose!");
+    // Else if the dealer has a score of 21:
+    // Print a message indicating the dealer got a Blackjack
+    console.log('Dealer got a Blackjack. You lose!');
+    // Call the endGame function
     endGame();
   }
 }
 
+// Define a function to end the game
 function endGame(playerCards, dealerCards, playerScore, dealerScore) {
-  console.log(`\nYour final cards: ${playerCards.join(", ")} (Score: ${playerScore})`);
-  console.log(`Dealer's final cards: ${dealerCards.join(", ")} (Score: ${dealerScore})`);
+  // Print the final cards and scores for both the player and dealer
+  console.log(`\nYour final cards: ${playerCards.join(', ')} (Score: ${playerScore})`);
+  console.log(`Dealer's final cards: ${dealerCards.join(', ')} (Score: ${dealerScore})`);
 
+  // If the player's score is equal to the dealer's score:
   if (playerScore === dealerScore) {
-    console.log("It's a tie!");
+    // Print a message indicating a tie
+    console.log('It\'s a tie!');
   } else if (dealerScore > 21 || (dealerScore < playerScore && playerScore <= 21)) {
-    console.log("Congratulations! You win!");
+    // Else if the dealer's score is greater than 21 or the player's score is greater than the dealer's score:
+    // Print a message indicating the player wins
+    console.log('Congratulations! You win!');
   } else {
-    console.log("Dealer wins. You lose!");
+    // Else:
+    // Print a message indicating the dealer wins
+    console.log('Dealer wins. You lose!');
   }
 
+  // Close the readline interface
   rl.close();
+  // Exit the process
   process.exit();
 }
 
-
+// Define the main function to play the game
 function playGame() {
+  // Initialize arrays for playerCards and dealerCards with two random cards each
   const playerCards = [getRandomCard(), getRandomCard()];
-  const dealerCards = [getRandomCard(), getRandomCard()];
-
-  let playerScore = calculateScore(playerCards);
-  let dealerScore = calculateScore(dealerCards);
-
-  displayGameStatus(playerCards, dealerCards, playerScore, dealerScore);
-  checkBlackjack(playerScore, dealerScore);
-
-  function promptUser() {
-    rl.question("Do you want to hit or stand? (h/s): ", (answer) => {
-      if (answer.toLowerCase() === "h") {
-        playerCards.push(getRandomCard());
-        playerScore = calculateScore(playerCards);
-
-        if (playerScore > 21) {
-          console.log("Bust! You went over 21. You lose!");
-          endGame(playerCards, dealerCards, playerScore, dealerScore);
-        } else {
-          displayGameStatus(playerCards, dealerCards, playerScore, dealerScore, false);
-          checkBlackjack(playerScore, dealerScore);
-          promptUser();
-        }
-      } else if (answer.toLowerCase() === "s") {
-        console.log("You chose to stand. Dealer's turn.");
-        dealerTurn();
-      } else {
-        console.log('Invalid input. Please enter "h" to hit or "s" to stand.');
-        promptUser();
-      }
-
-      // Move checkBlackjack inside here
-      // checkBlackjack(playerScore, dealerScore);
-    });
-  }
-
-  function dealerTurn() {
-    while (dealerScore < 17) {
-      dealerCards.push(getRandomCard());
-      dealerScore = calculateScore(dealerCards);
-      console.log(`Dealer hits. Dealer's cards: ${dealerCards.join(", ")} (Score: ${dealerScore})`);
-    }
-
-    console.log("Dealer stands.");
-    displayGameStatus(playerCards, dealerCards, playerScore, dealerScore, true);
-    endGame(playerCards, dealerCards, playerScore, dealerScore);
-  }
-
-  promptUser();
-}
-
-console.log("Welcome to Blackjack!");
-playGame();
+  const dealerCards = [getRandomCard(), getRandom
